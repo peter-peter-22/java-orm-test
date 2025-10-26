b9b24f4157c96b2664f3d32ff27c115847e6e39b
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-10-26 12:28:52 +0100

    minor refractor

--- src/main/java/com/example/orm/Document.java
+++ src/main/java/com/example/orm/Document.java
@@ -2,6 +2,7 @@ package com.example.orm;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Array;
 import org.hibernate.annotations.JdbcTypeCode;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Table(name = "documents")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,8 +27,6 @@ public class Document {
     @JdbcTypeCode(SqlTypes.VECTOR)
     @Array(length = 5)
     private float[] embedding;
-
-    public Document() {}
 
     public Document(String content, float[] embedding) {
         this.content = content;
